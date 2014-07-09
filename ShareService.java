90374878222204138510f24a0979d5725b17887f
sandeep1807 <sandeepbhati1807@gmail.com>
2014-07-09 11:27:15 +0530

    Create ShareService.java
    This is a interface which have method readCSVFile

---
+++ ShareService.java
@@ -0,0 +1,11 @@
+package com.binary;
+
+/*
+ * @author   : Sandeep Kumar
+ * @version  : 1.0
+ * @since    : 03-07-2014
+ */
+public interface ShareService {
+
+	public void readCSVFile(String fileName);
+}
